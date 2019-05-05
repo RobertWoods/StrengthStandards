59c57fa3526d3a2f0053a688d2a8b5f97a71039f
Robert Woods <robertwoods2013@gmail.com>
2019-05-05 19:48:24 -0400

    Adds ability for user to add their login credentials and retrieve a token

--- src/MyAppBar.js
+++ src/MyAppBar.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withStyles, AppBar, Toolbar, Typography, Button, Modal, Paper, TextField } from '@material-ui/core';
 import { lightGreen200 } from 'material-ui/styles/colors';
 
 function MyAppBar(props) {
+
+    const [modalopen, setModalOpen] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const updateUsername = (e) => setUsername(e.target.value);
+    const updatePassword = (e) => setPassword(e.target.value);
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
+
     const logout = () => {
         localStorage.removeItem('token');
         props.handler(false);
     }
 
-    const login = () => {
+    const getToken = () => {
+        setModalOpen(false);
         fetch('http://127.0.0.1:8000/api-token-auth/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                username: 'x',
-                password: 'x'
+                username: username,
+                password: password
             })
         })
             .then((result) => result.json())
             .then(result => {
+                console.log(result.token);
                 localStorage.setItem('token', result.token);
                 props.handler(true);
             })
@@ -34,17 +47,17 @@ function MyAppBar(props) {
                 <Typography className={classes.appBarTitle} component="h2" variant="h5" align="center" color="inherit" noWrap>
                     Strength Standards
                     </Typography>
-                <Button variant="outlined" size="small" onClick={props.loggedIn ? logout : login}>
+                <Button variant="outlined" size="small" onClick={props.loggedIn ? logout : openModal}>
                     {props.loggedIn ? "Log Out" : "Log In"}
                 </Button>
             </Toolbar>
-            <Modal open={!props.loggedIn}>
+            <Modal open={modalopen}>
                 <Paper className={classes.modalPaper}>
-                    <TextField label="email" type="email" />
-                    <TextField label="password" type="password" />
+                    <TextField onChange={updateUsername} label="username" type="text" />
+                    <TextField onChange={updatePassword} label="password" type="password" />
                     <br /><br />
-                    <Button onClick={login} className={classes.button} variant="contained">Login</Button>
-                    <Button onClick={login} className={classes.button} variant="contained">Cancel</Button>
+                    <Button onClick={getToken} className={classes.button} variant="contained">Login</Button>
+                    <Button onClick={closeModal} className={classes.button} variant="contained">Cancel</Button>
                 </Paper>
             </Modal>
         </AppBar>
